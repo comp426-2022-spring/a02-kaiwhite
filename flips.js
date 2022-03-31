873775c003a628fc7593abc743e53c27a0aa7db3
Kai White <kaiwhite@live.unc.edu>
2022-03-31 14:58:33 -0400

    try again

--- flips.js
+++ flips.js
@@ -4,11 +4,13 @@ var args = process.argv.slice(2);
 let coinFlip = coinFlips(args[2]);
 let countFlip = countFlips(coinFlip);
 
-coinFlip = coinFlips(1);
-if (coinFlip == 'tails'){
-    countFlip = {tails:1};
-} else {
-    countFlip = {heads:1};
+if (args[2] == null){
+    coinFlip = coinFlips(1);
+    if (coinFlip == 'tails'){
+        countFlip = {tails:1};
+    } else {
+        countFlip = {heads:1};
+    }
 }
 console.info(coinFlip);
 console.log(countFlip);
