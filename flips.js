7d96a2bbda7196354f71cdad8a4eb0a8a3c8c4ea
Kai White <kaiwhite@live.unc.edu>
2022-04-10 14:59:22 -0400

    flips

--- flips.js
+++ flips.js
@@ -1,10 +1,20 @@
 import {coinFlip, countFlips, coinFlips} from './modules/coin.mjs';
-import minimist from 'minimist'
 
-var myargs1 = minimist(process.argv.slice(2))
-const value = myargs1.number || 1
+if (process.argv.length < 3){
+    console.log(coinFlip());
+} else {
+    var myargs1 = process.argv.slice(2);
+    var myArgs = myargs1[0].charAt(9);
+    console.log(myArgs);
 
-let num = coinFlips(value)
-
-console.log(num)
-console.log(coinFlips(num))
+    var x = 10;
+    while (x < myargs1[0].length){
+        myArgs = myArgs + myargs1[0].charAt(x);
+        x++;
+    }
+    var num = parseInt(myArgs);
+    var coins = coinFlips(num);
+    console.log(coins);
+    var num = countFlips(coins);
+    console.log(num);
+}
